f936197ede67349bb6b2d3869d6a5c15fc79d523
Aruna <aruna.evan15@gmail.com>
2022-01-25 14:54:28 -0800

    Solution

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,9 +1,12 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  let next = findShortestStringRecursive(arr.slice(1))
+  return arr[0].length <= next.length ? arr[0] : next;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 'a'");
   console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
 
@@ -16,9 +19,34 @@ if (require.main === module) {
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+
+  console.log("");
+
+  console.log("Expecting: 'b'");
+  console.log("=>", findShortestStringRecursive(['b']));
+
+  console.log("");
+
+  console.log("Expecting: 'rose'");
+  console.log("=>", findShortestStringRecursive(['rose', 'rose']));
+
 }
 
 module.exports = findShortestStringRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * Find shortest string in an array using recursion.
+ * 
+ * 2. tests
+ * 
+ * 3. pseudocode
+ * 
+ * When down to the end, array will have 2 elements. Compare the two and return the shorter.
+ * One level up, compare those two and return the shorter, always returning the novel one if equal length.
+ * 
+ * 4. code (above)
+ * 5. refactor
+ * 6. optimize given time
+ */
